7976a22cccc07718d36a286737d138b996e88c49
Nabide Yusuf <y.nabide@alustudent.com>
2025-02-05 12:03:24 +0200

    Change of PID info text.

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "what devide do you want to use? " DEVICE
-echo "This process has a PID of $$"
+echo "This script is running on a process with PID: $$"
 while [[ 1 -lt 2 ]]
 do 
 	echo "$(date '+%Y-%m-%d %H:%M:%S') $DEVICE $(shuf -i 0-100 -n 1)" >> heart_rate_log.txt
