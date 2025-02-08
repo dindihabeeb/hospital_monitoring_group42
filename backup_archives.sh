e444f1236b002d31286302b0eb9b170caa8cfecd
Habeeb Dindi <h.dindi@alustudent.com>
2025-02-08 15:59:47 +0200

    Enhance backup archives script with directory creation and error handling

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p archive_logs_group42
 mv heart_rate_log.txt[[:digit:]]*[[:digit:]] archive_logs_group42/
 scp -r archive_logs_group42/ f6314d11c866@f6314d11c866.1009380b.alu-cod.online:~/
