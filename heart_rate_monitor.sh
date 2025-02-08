232344a6567c3fc88a89fd4f4d237d99c2e6b733
Habeeb Dindi <h.dindi@alustudent.com>
2025-02-08 15:55:54 +0200

    Improve heart rate monitoring script

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -4,4 +4,5 @@ echo "This script is running on a process with PID: $$"
 while [[ 1 -lt 2 ]]
 do 
 	echo "$(date '+%Y-%m-%d %H:%M:%S') $DEVICE $(shuf -i 0-100 -n 1)" >> heart_rate_log.txt
+	sleep 1
 done
