166936069c0f4b0ead344581e3fbbaf382ff884e
TRolande <r.tumugane@alustudent.com>
2025-02-08 08:59:54 -0800

    add the command description

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#mkdir -p creates the directory first unless it did n't existed before moving the archived files
 mkdir -p archive_logs_group42
 mv heart_rate_log.txt[[:digit:]]*[[:digit:]] archive_logs_group42/
 scp -r archive_logs_group42/ f6314d11c866@f6314d11c866.1009380b.alu-cod.online:~/
