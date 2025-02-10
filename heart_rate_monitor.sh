55274d11aa753c75742cd79aad086e5bdd7589e2
blessiingab <b.ingabire1@alustudent.com>
2025-02-10 01:02:43 -0800

    Added more comments

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -4,9 +4,12 @@ i#!/bin/bash
 read -p "what devide do you want to use? " DEVICE
 # Display the process ID of the script
 echo "This script is running on a process with PID: $$"
+# Start an infinite loop to continuously log heart rate data
 while [[ 1 -lt 2 ]]
 do
-	# Log current timestamp, device name, and random heart rate value (0-100)	
+	# Log current timestamp, device name, and random heart rate value (0-100)
+        # The random value is generated using the 'shuf' command	
 	echo "$(date '+%Y-%m-%d %H:%M:%S') $DEVICE $(shuf -i 0-100 -n 1)" >> heart_rate_log.txt
+	# Pause for 1 second before the next iteration to prevent excessive logging
 	sleep 1
 done
