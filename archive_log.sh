c2bc5f11da24134714c20a31d7e0156d605d3686
davy-mgr <d.mugire@alustudent.com>
2025-02-10 02:02:51 -0800

    made archive script copy heart_rate_log.txt into the archives directory

--- archive_log.sh
+++ archive_log.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #cp it copies files and directories from one location to another
-cp heart_rate_log.txt heart_rate_log.txt_$(date '+%Y%m%d_%H%M%S')
+cp heart_rate_log.txt archive_logs_group42/heart_rate_log.txt_$(date '+%Y%m%d_%H%M%S')
