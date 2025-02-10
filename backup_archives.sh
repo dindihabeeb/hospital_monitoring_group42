19f52862c2cc17a753aeb706a84f098295ecd085
TRolande <r.tumugane@alustudent.com>
2025-02-10 00:04:01 -0800

    add the comments

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #mkdir -p creates the directory first unless it did n't existed before moving the archived files
+#scp -r it securely copies files over SSH and copies directories recursively including all files and subdirectories
 mkdir -p archive_logs_group42
 mv heart_rate_log.txt[[:digit:]]*[[:digit:]] archive_logs_group42/
 scp -r archive_logs_group42/ f6314d11c866@f6314d11c866.1009380b.alu-cod.online:~/
