5cbd72ef9246d42d4f754d436e761125feedb7c0
blessiingab <b.ingabire1@alustudent.com>
2025-02-10 00:41:30 -0800

    Move logs and added SCP transfer

--- backup_archives.sh
+++ backup_archives.sh
@@ -2,6 +2,7 @@
 
 #mkdir -p creates the directory first unless it did n't existed before moving the archived files
 #scp -r it securely copies files over SSH and copies directories recursively including all files and subdirectories
+# Move log files with digits in their names to the archive directory
 mkdir -p archive_logs_group42
 mv heart_rate_log.txt[[:digit:]]*[[:digit:]] archive_logs_group42/
 scp -r archive_logs_group42/ f6314d11c866@f6314d11c866.1009380b.alu-cod.online:~/
